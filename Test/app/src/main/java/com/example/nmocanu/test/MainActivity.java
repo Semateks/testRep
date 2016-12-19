b088caa1f406fa1ee9c96e3d6b2f5e1d979eb02b
Nicoleta Mocanu <nicoleta.mocanu@windriver.com>
2016-12-19 11:36:07 +0200

    Changes 19dec

--- Test/app/src/main/java/com/example/nmocanu/test/MainActivity.java
+++ Test/app/src/main/java/com/example/nmocanu/test/MainActivity.java
@@ -1,17 +1,31 @@
+/*********************************************************
+ * Copyright (c) 2016 Nicoleta Mocanu
+ * All rights reserved. You may not copy, distribute, publicly display,
+ * create derivative works from or otherwise use or modify this
+ * software without first obtaining written agreement from Nicoleta Mocanu
+ *********************************************************/
+
 package com.example.nmocanu.test;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 
     ImageView thePic;
     Button upload;
+
+    private static int RESULT_LOAD_IMAGE = 1;
 
 
     @Override
@@ -21,17 +35,39 @@ public class MainActivity extends Activity {
 
         thePic= (ImageView) findViewById(R.id.bitchView);
         thePic.setImageResource(R.drawable.bitchslap);
+        thePic.setTag("0");
 
         upload = (Button) findViewById(R.id.upload);
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SlapActivity.class);
-                startActivity(intent);
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images
+                        .Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+
             }
         });
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            thePic.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+            thePic.setTag("UpdatedPic");
+            upload.setVisibility(View.INVISIBLE);
+            Toast.makeText(MainActivity.this, "Mergeeeeee", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 
 
--- Test/app/src/main/java/com/example/nmocanu/test/SlapActivity.java
+++ Test/app/src/main/java/com/example/nmocanu/test/SlapActivity.java
@@ -1,15 +1,51 @@
+/*********************************************************
+ * Copyright (c) 2016 Nicoleta Mocanu
+ * All rights reserved. You may not copy, distribute, publicly display,
+ * create derivative works from or otherwise use or modify this
+ * software without first obtaining written agreement from Nicoleta Mocanu
+ *********************************************************/
+
 package com.example.nmocanu.test;
 
 import android.app.Activity;
-import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by nmocanu on 17.12.2016.
  */
 public class SlapActivity extends Activity {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_slap, menu);
+        return true;
+    }
+
+    /**
+     * Event Handling for Individual menu item selected
+     * Identify single menu item by it's id
+     * */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        switch (item.getItemId())
+        {
+            case R.id.menu_bookmark:
+                // Single menu item is selected do something
+                // Ex: launching new activity/screen or show alert message
+                Toast.makeText(this, "Bookmark is Selected", Toast
+                        .LENGTH_SHORT).show();
+                return true;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
