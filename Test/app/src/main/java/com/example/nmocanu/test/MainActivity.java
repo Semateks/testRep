b7b22156d19059ffc4cb778a9e3b1df06dc7fc81
Nicoleta Mocanu <nicoleta.mocanu@windriver.com>
2016-12-19 08:59:37 +0200

    Changes 18dec

--- Test/app/src/main/java/com/example/nmocanu/test/MainActivity.java
+++ Test/app/src/main/java/com/example/nmocanu/test/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.nmocanu.test;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+
+    ImageView thePic;
+    Button upload;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        thePic= (ImageView) findViewById(R.id.bitchView);
+        thePic.setImageResource(R.drawable.bitchslap);
+
+        upload = (Button) findViewById(R.id.upload);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SlapActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
---
+++ Test/app/src/main/java/com/example/nmocanu/test/SlapActivity.java
@@ -0,0 +1,15 @@
+package com.example.nmocanu.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nmocanu on 17.12.2016.
+ */
+public class SlapActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
